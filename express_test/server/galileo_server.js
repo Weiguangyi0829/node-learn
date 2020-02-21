cb4abbc48a9905b2baa88a0855bc1858e89b01b3
Weiguangyi0829 <wgy7413@163.com>
2020-02-21 17:04:46 +0800

    1

--- express_test/server/galileo_server.js
+++ express_test/server/galileo_server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const path = require('path');//检查操作系统来确定路径正反斜杠的写法
 const bodyParser = require('body-parser');//express里面已经有可以直接用
 
-console.log(__dirname);
-
 const app = express();
 
 app.use(bodyParser.json());//解析转化提交上来的 json 数据，解析为 js 对象
