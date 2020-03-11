2bc43432425ffafdb5f1ff8ac575b5965946d9d6
Weiguangyi0829 <wgy7413@163.com>
2020-03-11 12:31:25 +0800

    mongo.js

---
+++ web_server/server/mongo.js
@@ -0,0 +1,25 @@
+const mongodb = require('mongodb');
+const MC = mongodb.MongoClient;
+const dbUrl = 'mongodb://localhost:27017';
+
+let storeDB;
+
+function getStoreDB(callback){
+    if(storeDB){
+        callback(storeDB);
+        return;
+    }
+    MC.connect(dbUrl,function(err,result){
+        if(err){
+            console.log(err);
+            return;
+        }
+        console.log('连接成功');
+        storeDB = client.db('store');
+        callback(storeDB)
+        
+    });
+}
+module.exports ={
+    getStoreDB
+}
